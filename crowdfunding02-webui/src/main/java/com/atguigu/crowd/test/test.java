d5945f842c97eafcddc2fa9254b446c7fd809c06
PanX <7787489+PanXJin@user.noreply.gitee.com>
2021-05-08 16:07:06 +0800

    P35  5:38

--- crowdfunding02-webui/src/main/java/com/atguigu/crowd/test/test.java
+++ crowdfunding02-webui/src/main/java/com/atguigu/crowd/test/test.java
@@ -3,6 +3,8 @@ package com.atguigu.crowd.test;
 import com.atguigu.crowd.entity.Admin;
 import com.atguigu.crowd.mapper.AdminMapper;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.junit.runner.RunWith;
@@ -24,10 +26,28 @@ public class test {
     private AdminMapper adminMapper;
 
     @Test
+    public void textLog() {
+        //1、获取logger对象,通常传入的class对象就是当前打印日志的类
+        Logger logger = LoggerFactory.getLogger(test.class);
+        //2、根据不同日志级别打印日志
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+    }
+
+    @Test
     public void textAdmin() {
         Admin admin = new Admin(null, "Panx", "123", "PanX", "123@qq.com", null);
         int count = adminMapper.insert(admin);
-        System.out.println("受影响的行数："+count);
+        /*
+         * 如果在实际开发中查看数值都是用out会消耗很多性能，因为out是IO操作
+         * 即使上线前花时间删除代码重的out也可能会有遗漏且麻烦
+         * 如果使用日志就可以通过日志级别就可以进行批量的打印*/
+//        System.out.println("受影响的行数："+count);
+        Logger logger = LoggerFactory.getLogger(test.class);
+        //2、根据不同日志级别打印日志
+        logger.error("受影响的行数："+count);
     }
 
     @Test
